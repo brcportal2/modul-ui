5b0a3564fcef83a0591c8d9ce168d8cce414e9e1
Vedmak Sergey <serg.vedmak@gmail.com>
2018-10-16 17:52:57 +0300

    add DateFormat stories

--- stories/Format/index.js
+++ stories/Format/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {AmountFormat} from '../../source/AmountFormat';
 import {NumberFormat} from '../../source/NumberFormat';
-import {select, text, number} from '@storybook/addon-knobs';
+import {DateFormat} from '../../source/DateFormat';
+import {select, text, number, date} from '@storybook/addon-knobs';
+
+const defaultDateValue = new Date('Jan 20 2017');
+
+function myDateKnob(name, defaultValue) {
+	const stringTimestamp = date(name, defaultValue);
+	return new Date(stringTimestamp);
+}
 
 export default storiesOf('Format', module)
 	.add('AmountFormat', () =>
@@ -30,6 +38,14 @@ export default storiesOf('Format', module)
 	.add('NumberFormat', () =>
 		<NumberFormat
 			value={text('value', '123456')}
+			def={text('def', 'empty message')}
+		/>
+	)
+
+	.add('DateFormat', () =>
+		<DateFormat
+			value={myDateKnob('value', defaultDateValue)}
+			format={text('format', '')}
 			def={text('def', 'empty message')}
 		/>
 	);
