b98eafd72db3fc54fed9457c6200b4fcb037eca7
Vedmak Sergey <serg.vedmak@gmail.com>
2018-10-16 15:24:49 +0300

    refactor and stories button component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,28 @@
+import React from 'react';
+import PropTable from '@storybook/addon-info/dist/components/PropTable';
 import {addDecorator, configure} from '@storybook/react';
 import {withKnobs} from '@storybook/addon-knobs';
 import {withOptions} from '@storybook/addon-options';
+import {withInfo} from '@storybook/addon-info';
+
+const PropTypesTable = ({propDefinitions, ...props}) => {
+	propDefinitions.forEach((def) => {
+	  if (typeof def.propType === 'string') {
+		def.propType = {name: def.propType};
+	  }
+	});
+
+	return <PropTable propDefinitions={propDefinitions} {...props} />;
+};
+
 
 addDecorator(withKnobs);
+
+addDecorator(withInfo({
+	inline: true,
+	header: false,
+	TableComponent: PropTypesTable // костыль, так как есть баг с отображением propTypes https://github.com/storybooks/storybook/issues/2708
+}));
 
 addDecorator(withOptions({
 	addonPanelInRight: true
--- stories/Button/index.js
+++ stories/Button/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {Button} from '../../source/Button';
-import {boolean} from '@storybook/addon-knobs';
+import {boolean, select, text} from '@storybook/addon-knobs';
 
 export default storiesOf('Button', module)
-	.add('default example', () =>
+	.add('default', () =>
 		<Button
 			loading={boolean('loading', false)}
 			disabled={boolean('disabled', false)}
+
+			mdStyle={select(
+				'mdStyle',
+
+				[
+					'',
+					'second',
+					'cancel',
+					'dark',
+					'white',
+					'clean',
+					'grey',
+					'light',
+					'light_primary',
+					'light-white',
+					'_gd-orange',
+				],
+
+				''
+			)}
+
+			mdSize={select(
+				'mdSize',
+
+				[
+					'',
+					'xxsmall',
+					'xsmall',
+					'small',
+					'middle',
+					'full',
+					'wide',
+				],
+
+				''
+			)}
+
+			icon={text('icon', '')}
 		>
 			Hello Button
 		</Button>
