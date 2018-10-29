620bb165bd70a6e2624f2fb8cf9ab5651f7de609
rainur102 <rainur102@gmail.com>
2018-10-29 13:33:54 +0500

    PV-26136 вынесен jquery-datepicker

--- source/DatePicker/DatePicker.jsx
+++ source/DatePicker/DatePicker.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {validateHelper} from "modul-helpers";
-import 'jquery-datetimepicker/build/jquery.datetimepicker.full.min';
+import 'modul-jquery-datetimepicker';
 
 const datePickerOptions = [
 	'value',
