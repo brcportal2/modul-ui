8438086b8b8306ded163c6649aaa8cf39f58c849
G.Ilshat <gaifullin.ilshat@gmail.com>
2018-11-20 11:53:43 +0500

    Добавил Popup в который флаг отображения можно передавать снаружи

--- source/Popup/index.js
+++ source/Popup/index.js
@@ -1,11 +1,13 @@
 import ModalPopup from './ModalPopup';
 import ContentPopup from './ContentPopup';
 import ConfirmPopup from './ConfirmPopup';
+import Popup from './Popup';
 
 
 export default ContentPopup;
 export {
 	ModalPopup,
 	ContentPopup,
-	ConfirmPopup
+	ConfirmPopup,
+	Popup,
 };
