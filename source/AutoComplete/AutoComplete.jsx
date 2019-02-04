2489eb815c03a40c7e0b1d9c9a7f86f4d3bb3287
Пасько Даниэль Игоревич <d.pasko@modulbank.ru>
2019-02-04 12:17:53 +0500

    Стили auto-complete

--- source/AutoComplete/AutoComplete.jsx
+++ source/AutoComplete/AutoComplete.jsx
@@ -1,4 +1,5 @@
-import 'antd/lib/auto-complete/style/css';
+// import 'antd/lib/auto-complete/style/css';
+import './auto-complete.styl';
 import AutoComplete from 'antd/lib/auto-complete/index';
 
 export default AutoComplete;
