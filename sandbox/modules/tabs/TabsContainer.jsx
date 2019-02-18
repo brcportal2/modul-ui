6c49a48c12e5e178ac41787882f5c2ce980751f5
Roman Balandin <tinkerscript@yandex.ru>
2019-02-18 13:25:37 +0500

    Апгрейд примера Tabs в песочнице

--- sandbox/modules/tabs/TabsContainer.jsx
+++ sandbox/modules/tabs/TabsContainer.jsx
@@ -10,16 +10,16 @@ export default class TabsContainer extends Component {
 					<div className="col-4">
 						<Tabs>
 							<Tabs.TabPane tab="Сегодня" key="Day">
-								tab content
+								tab content 1
 							</Tabs.TabPane>
 							<Tabs.TabPane tab="Спринт" key="Sprint">
-								tab content
+								tab content 2
 							</Tabs.TabPane>
 							<Tabs.TabPane tab="Месяц" key="Month">
-								tab content
+								tab content 3
 							</Tabs.TabPane>
 							<Tabs.TabPane tab="Всё время" key="All">
-								tab content
+								tab content 4
 							</Tabs.TabPane>
 						</Tabs>
 					</div>
